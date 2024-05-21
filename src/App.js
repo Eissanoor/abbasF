cc950a1d55ded465f16368518ecf61949331988f
Eissanoor <eissaanoor@gmail.com>
2024-05-21 21:00:20 +0500

    upadted

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Pagerounter from './Component/Rounter/Pagerounter';
 import axios from "axios";
 
-axios.defaults.baseURL = "https://tan-courageous-crow.cyclic.app/";
+axios.defaults.baseURL = "http://161.97.139.96:3050/";
 function App() {
   return (
     <div className="App">
