0e1fe3822888516aa02168339de3a97583687855
Eissanoor <eissaanoor@gmail.com>
2024-05-22 06:42:55 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Pagerounter from './Component/Rounter/Pagerounter';
 import axios from "axios";
 
-axios.defaults.baseURL = "http://161.97.139.96:3050/";
+axios.defaults.baseURL = "https://161.97.139.96:3050/";
 function App() {
   return (
     <div className="App">
