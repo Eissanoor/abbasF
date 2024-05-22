ff1c71051d54fa689f6c3e5966dc8f2cd8ec8d7b
Eissanoor <eissaanoor@gmail.com>
2024-05-22 16:09:00 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Pagerounter from './Component/Rounter/Pagerounter';
 import axios from "axios";
 
-axios.defaults.baseURL = "https://161.97.139.96:3050/";
+axios.defaults.baseURL = "https://abbas.vercel.app/";
 function App() {
   return (
     <div className="App">
